b4ac46562638e9f4e89030be35ca65d44ef0a6a9
ez0603 <dlwldjs3132@naver.com>
2024-09-03 15:33:34 +0900

    Feat : check

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient(); // Query의 저장공간
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
-    <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-            <BrowserRouter  basename="/Portfolio_ez"> 
-           
-                <App/>
-            </BrowserRouter>
-        </RecoilRoot>
-    </QueryClientProvider>
-    
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      {/* <BrowserRouter  basename="/Portfolio_ez">  */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
+  </QueryClientProvider>
 );
 
 reportWebVitals();

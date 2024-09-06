e1c68431a0b9d75a61981398a80317391b66f835
ez0603 <dlwldjs3132@naver.com>
2024-09-07 06:06:51 +0900

    Fix : Aboutme List CSS 수정

--- src/components/Modal/AboutMeModal/style.js
+++ src/components/Modal/AboutMeModal/style.js
@@ -54,7 +54,7 @@ export const modalContentStyle = (isClosing) => css`
 
   @media (max-width: 700px) {
     width: 90%;
-    height: 95vh;
+    height: 85vh;
     max-width: none;
     max-height: none;
   }
